cfc034910345c8174a45d0df7382b6ab74ab0724
sachit6602 <freakysachit.sr@gmail.com>
2024-11-20 19:23:57 +0000

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => (
         element={
           <>
             <LandingPage />
-            <HeroSection />
+            <HeroSection /> 
 
             <ProductsSection products={sampleProducts} />
           </>
